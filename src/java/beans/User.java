24187ca4ca3bfd2314b6f835b4f378039cc4ae4e
c0665336 <c0665336@N208-F15-004.lambton.on.ca>
2016-03-17 11:47:59 -0400

    Created post.java & user.java

--- src/java/beans/User.java
+++ src/java/beans/User.java
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+// gurvinder kaur
 package beans;
 
 import javax.faces.bean.ManagedBean;
